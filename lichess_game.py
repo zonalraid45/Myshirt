ba73884eab9a76263c0f46a60cce93a0d4c3bc44
zonalraid45 <96386317+zonalraid45@users.noreply.github.com>
2025-09-17 21:54:55 +0530

    Update lichess_game.py

--- lichess_game.py
+++ lichess_game.py
@@ -968,7 +968,9 @@ class Lichess_Game:
 
         if self.config.online_moves.chessdb.enabled:
             if not self.config.online_moves.chessdb.only_without_book or not self.book_settings.readers:
-                if self.board.uci_variant == 'chess':
+                if self.config.online_moves.chessdb.fromposition and self.game_info.variant != Variant.FROM_POSITION:  
+                    pass
+                elif self.board.uci_variant == 'chess':
                     opening_sources[self._make_chessdb_move] = self.config.online_moves.chessdb.priority
 
         move_sources += [opening_source
